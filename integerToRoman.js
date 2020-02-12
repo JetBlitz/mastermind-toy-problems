77b572cbe4d2ef1c8a65d82e84376f9b29b93784
Eivin Adlawan <eadlawa2@gmail.com>
2020-02-11 23:01:03 -0600

    First commit

---
+++ integerToRoman.js
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/integer-to-roman/
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+
+var DIGIT_VALUES = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
+
+var intToRoman = function(num) {
+
+};
