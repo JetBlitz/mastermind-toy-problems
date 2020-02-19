7e27f08397c3772dceee3c9c414f6aa8e4f43983
Eivin Adlawan <eadlawa2@gmail.com>
2020-02-19 11:37:26 -0600

    Initial commit, prompt with starter code

---
+++ inPlaceShuffleList.js
@@ -0,0 +1,25 @@
+/**
+Write a function for doing an in-place ↴ shuffle of a list.
+
+The shuffle must be "uniform," meaning each item in the original list must have the same probability of ending up in each spot in the final list.
+
+Assume that you have a function get_random(floor, ceiling) for getting a random integer that is >= floor and <= ceiling.
+
+*/
+
+function getRandom(floor, ceiling) {
+  return Math.floor(Math.random() * (ceiling - floor + 1)) + floor;
+}
+
+function shuffle(array) {
+
+  // Shuffle the input in place
+
+
+}
+
+
+const sample = [1, 2, 3, 4, 5];
+console.log('Initial array: ', sample);
+shuffle(sample);
+console.log('Shuffled array: ', sample);
