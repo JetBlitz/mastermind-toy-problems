fea87629677e0a7cc0117edb0f8b898d2bdd3bdc
Eivin Adlawan <eadlawa2@gmail.com>
2020-03-10 11:30:46 -0500

    Savepoint: added story prompt

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -7,6 +7,16 @@ function mergeArrays(myArray, alicesArray) {
 }
 
 
+/** Example:
+ * In order to win the prize for most cookies sold, my friend Alice and I are going to merge our Girl Scout Cookies orders and enter as one unit.
+
+Each order is represented by an "order id" (an integer).
+
+We have our lists of orders sorted numerically already, in arrays. Write a function to merge our arrays of orders into one sorted array.
+
+For example:
+ */
+
 const myArray = [3, 4, 6, 10, 11, 15];
 const alicesArray = [1, 5, 8, 12, 14, 19];
 
