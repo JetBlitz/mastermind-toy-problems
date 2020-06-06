b3ca16be88c207de66f5fae84f10ccda5a9afb95
Eivin Adlawan <eadlawa2@gmail.com>
2020-06-06 17:27:28 -0500

    Added question prompt to intial code

--- simulate7SidedDie.js
+++ simulate7SidedDie.js
@@ -1,3 +1,9 @@
+/**
+ * You have a function rand5() that generates a random integer from 1 to 5. Use it to write a function rand7() that generates a random integer from 1 to 7.
+
+rand5() returns each integer with equal probability. rand7() must also return each integer with equal probability.
+ */
+
 function rand5() {
   return Math.floor(Math.random() * (5 - 1 + 1)) + 1;
 }
